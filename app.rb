56478bce50f83a97d48568911f7d0eb9fb244af3
Stephen Reid <stephen@stephenreid.net>
2020-03-05 16:14:39 +0000

    .

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra' 
+require 'open-uri' 
 
 get '/*' do
-  redirect params[:splat] && ENV[params[:splat].first.upcase] ? ENV[params[:splat].first.upcase] : ENV['REDIRECT_URL']
+  url =  (params[:splat] && ENV[params[:splat].first.upcase]) ? ENV[params[:splat].first.upcase] : ENV['REDIRECT_URL']
+  if URI(url).host == 'www.dropbox.com'
+    open(url).read.force_encoding('utf-8')
+  else
+    redirect url
+  end
 end
